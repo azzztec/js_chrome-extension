0a0651bb0927fe6b5d9893f436a4a78f61755103
raury <33kaxa@gmail.com>
2020-11-24 20:06:24 +0300

    feat: add popup logic

--- popup.js
+++ popup.js
@@ -1,3 +1,54 @@
-chrome.storage.local.get("allPageText", function(data) {
+import { MOST_POPULAR_WORDS } from './config.js'
 
+chrome.storage.local.get("allPageText", function(data) {
+  let allPageText = data.allPageText
+  let wordCountBlock = document.querySelector('.wordCountBlock')
+  let uniqueWordCountBlock = document.querySelector('.wordCountBlock.unique')
+  let graphActiveBlock = document.querySelector('.graphActiveBlock')
+
+  wordCountBlock.innerText = `Number of words: ${countWords(allPageText)}`
+  uniqueWordCountBlock.innerText = `Number of unique words: ${countUniqueWords(allPageText)}`
+  graphActiveBlock.style.height = `${getTextPopularity(allPageText)}%`
 })
+
+function getTextPopularity(text) {
+  let mostPopularWordsList = MOST_POPULAR_WORDS.split(',')
+  let numberOfPopularWords = 0
+  let uniqueWordsList = getUniqueWords(text)
+
+  mostPopularWordsList.forEach(popularWord => {
+    if(uniqueWordsList.includes(popularWord)) {
+      numberOfPopularWords++
+    }
+  })
+  let percentOfPopularWords = Math.round((numberOfPopularWords / mostPopularWordsList.length) * 100)
+  return percentOfPopularWords
+}
+
+function getAllWords(text) {
+  let wordsList = text.split(' ')
+  return wordsList
+}
+
+function countWords(text) {
+  let wordsNumber = getAllWords(text).length
+  return wordsNumber
+}
+
+function getUniqueWords(text) {
+  let uniqueWordsList = []
+  let wordsList = getAllWords(text)
+
+  wordsList.forEach(word => {
+    if(!uniqueWordsList.includes(word)) {
+      uniqueWordsList.push(word)
+    }
+  });
+
+  return uniqueWordsList
+}
+
+function countUniqueWords(text) {
+  let uniqueWordsNumber = getUniqueWords(text).length
+  return uniqueWordsNumber
+}
